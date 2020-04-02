f937feee5fed2598985c590496fa652ad111a5b7
Marcelo Caldas <mcq1@cdc.gov>
2020-04-02 14:39:52 -0400

    adding ci

--- server/build.sh
+++ server/build.sh
@@ -5,12 +5,12 @@ docker rmi -f arln/$APPNAME
 docker rmi -f 471108701394.dkr.ecr.us-east-1.amazonaws.com/arln/$APPNAME
 docker build -t arln/$APPNAME .
 docker tag arln/$APPNAME:latest $REGISTRY/arln/$APPNAME:latest
-$(aws ecr get-login --no-include-email --region us-east-1)
+$(aws ecr get-authorization-token --region us-east-1)
 docker push $REGISTRY/arln/$APPNAME:latest
-pwd=$(aws ecr get-login --no-include-email --region us-east-1 | awk '{print $6}')
+pwd=$(aws ecr get-authorization-token --region us-east-1 | grep -o -E "\"authorizationToken\":.+"  | awk '{print $2}')
 
 kubectl delete secret eip-registry-secret
 kubectl create secret docker-registry eip-registry-secret --docker-server=https://$REGISTRY --docker-username="AWS" --docker-password=$pwd
-sed -e "s/<VERSION>/latest/g" ${APPNAME}-deployment.yaml > deployment.yaml
-kubectl delete -f deployment.yaml
-kubectl create -f deployment.yaml
+sed -e "s/<VERSION>/latest/g" ${APPNAME}-deployment.yaml > target/deployment.yaml
+kubectl delete -f target/deployment.yaml
+kubectl create -f target/deployment.yaml
